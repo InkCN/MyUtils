5180a123441c8c8f09cc34edbc9cb17864e63fe2
Ink <201462457@qq.com>
2020-07-28 22:46:03 +0800

    download by url

--- src/io/DownLoadByURL.java
+++ src/io/DownLoadByURL.java
@@ -6,7 +6,7 @@ import java.net.URL;
 
 /**
  * download by url
- * <p>
+ *
  * 防止屏蔽程序抓取而返回403错误可以使用以下语句：
  * conn.setRequestProperty("User-Agent","Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
  */
@@ -27,10 +27,12 @@ public class DownLoadByURL {
         BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
         BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path));
 
-        byte[] b = new byte[1024];
+        byte[] b = new byte[1024 * 4];
         int len;
         while ((len = bis.read(b)) > 0)
             bos.write(b, 0, len);
+        bos.close();
+        bis.close();
         return true;
     }
 }
