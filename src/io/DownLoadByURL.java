9e98d1ddf450c3a7bbb336e7b03c86a99db1f763
Ink <201462457@qq.com>
2020-07-28 22:41:48 +0800

    download by url

--- src/io/DownLoadByURL.java
+++ src/io/DownLoadByURL.java
@@ -1,7 +1,36 @@
 package io;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * download by url
+ * <p>
+ * 防止屏蔽程序抓取而返回403错误
+ * conn.setRequestProperty("User-Agent","Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
  */
 public class DownLoadByURL {
+
+    public static boolean downloadByURL(String fileUrl, String path) throws IOException {
+        URL url = new URL(fileUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        //set connect and read time can't over 5 seconds
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
+
+        int code = conn.getResponseCode();
+        if (code != HttpURLConnection.HTTP_OK)
+            return false;
+
+        BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path));
+
+        byte[] b = new byte[1024];
+        int len;
+        while ((len = bis.read(b)) > 0)
+            bos.write(b, 0, len);
+        return true;
+    }
 }
